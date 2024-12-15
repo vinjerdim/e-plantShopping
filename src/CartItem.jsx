25c69b1e966727d5f572aaae475d1f1aaf6a4578
vinjerdim <marjer17dim@gmail.com>
2024-12-14 20:39:33 -0500

    fix: add handler for checkout

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -20,6 +20,10 @@ const CartItem = ({ onContinueShopping }) => {
         onContinueShopping(e);
     };
 
+    const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+    };
+
     const handleIncrement = (item) => {
         dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
     };
@@ -62,7 +66,7 @@ const CartItem = ({ onContinueShopping }) => {
             <div className="continue_shopping_btn">
                 <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
                 <br />
-                <button className="get-started-button1">Checkout</button>
+                <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
             </div>
         </div>
     );
