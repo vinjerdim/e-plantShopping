e53f388111b20ad847ae90509d1c3211e6db6455
vinjerdim <marjer17dim@gmail.com>
2024-12-14 20:03:45 -0500

    fix: create handleIncrement, handleDecrement, handleRemove and calculateTotalCost

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,17 +21,20 @@ const CartItem = ({ onContinueShopping }) => {
     };
 
     const handleIncrement = (item) => {
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity + 1 }));
     };
 
     const handleDecrement = (item) => {
-
+        dispatch(updateQuantity({ name: item.name, quantity: item.quantity - 1 }));
     };
 
     const handleRemove = (item) => {
+        dispatch(removeItem(item.name));
     };
 
     // Calculate total cost based on quantity for an item
     const calculateTotalCost = (item) => {
+        return item.quantity * parseInt(item.cost.replace("$", ""));
     };
 
     return (
